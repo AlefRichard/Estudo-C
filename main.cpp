9e227d22a1db7484b7ca85b5b665fa861a8c3e82
alef12 <alef.richard@cmtecnologia.com.br>
2016-07-15 15:41:10 -0300

    Aulguns exercicios feitos para relembrar

---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <stdlib.h>
+#include <locale.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    // Aceita o acento
+    setlocale(LC_ALL,"");
+
+
+    //Aula 05
+
+    //int varInt = 100;
+    //char c = 'r';
+    //double pFlutuante = 5.99;
+
+    //cout << "O valor da variavel varInt é: " << varInt << endl;
+    //cout << "O valor da c varInt é: " << c << endl;
+    //cout << "O valor da pFlutuante varInt é: " << pFlutuante << endl;
+    //cout << endl;
+    //cout << "Memória da variavel varInt: " << sizeof(varInt) << "bytes" << endl;
+    //cout << "Memória da variavel c: " << sizeof(c) << "bytes" << endl;
+    //cout << "Memória da variavel pFlutuante: " << sizeof(pFlutuante) << "bytes" << endl;
+
+
+
+    //Aula 12
+    //
+    //int varInt = 100;
+    //char c = 'r';
+    //double pFlutuante = 5.99;
+
+    //cout << "O valor da variavel varInt é: " << varInt << endl;
+    //cout << "O valor da c varInt é: " << c << endl;
+    //cout << "O valor da pFlutuante varInt é: " << pFlutuante << endl;
+    //cout << endl;
+    //cout << "Memória da variavel varInt: " << sizeof(varInt) << "bytes" << endl;
+    //cout << "Memória da variavel c: " << sizeof(c) << "bytes" << endl;
+    //cout << "Memória da variavel pFlutuante: " << sizeof(pFlutuante) << "bytes" << endl;
+
+
+
+    // Aula 14 - STREAM
+    // Obj CIN
+    // Obj COUT
+
+    //cout << "Estudo sobre a entrada e saída de dados." << endl;
+    //cout << setw(10) << 1 << endl;
+    //cout << setw(10) << 2 << endl;
+    //cout << setw(10) << 3 << endl;
+    //cout << setw(10) << 4 << endl;
+
+
+
+    // Aula 15
+    // Manipulação de dados
+
+    //cout << "Informe um número" << endl;
+    //int num1 = 0;
+    //cin >> num1;
+
+    //cout << "Informe outro número" << endl;
+    //int num2 = 0;
+    //cin >> num2;
+
+    //cout << "A soma dos resultados é: " << num1 + num2 << endl;
+
+
+
+    //Aula 16
+    // Modificadores de Tipo
+
+    //int a = 0; //4 bytes
+    //short int b = 0; // 2 bytes
+    //long int c = 0; // 4 bytes
+
+    //int a2 = 0;
+    //signed int b2 = -10;
+    //unsigned int c2 = -10;
+
+    //cout << sizeof(a) << " - " << a2 << endl;
+    //cout << sizeof(b) << " - " << b2 << endl;
+    //cout << sizeof(c) << " - " << c2 << endl;
+
+    //char caractere = 'c';
+
+
+    system("pause");
+    return 0;
+}
