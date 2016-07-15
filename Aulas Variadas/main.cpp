435555f6dfcbe207e6dd4ed0607e9af1542e4843
alef12 <alef.richard@cmtecnologia.com.br>
2016-07-15 16:15:21 -0300

    continuar a partir da 18

---
+++ Aulas Variadas/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+#include <locale.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    // Serve para aceitar acentuação
+    setlocale(LC_ALL,"");
+
+    //continuar aula 18
+
+
+    system("pause");
+    return 0;
+}
