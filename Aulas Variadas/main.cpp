cd680c5e7ee59a2e4a590bd4e73d35d8dc5a153c
alef12 <alef.richard@cmtecnologia.com.br>
2016-07-18 16:40:50 -0300

    Algumas aulas novas

--- Aulas Variadas/main.cpp
+++ Aulas Variadas/main.cpp
@@ -10,7 +10,74 @@ int main()
     // Serve para aceitar acentuação
     setlocale(LC_ALL,"");
 
-    //continuar aula 18
+    //Aula 18
+    // Calculadora simples
+
+    //int num1, num2;
+
+    //cout << "Vamos lá na nossa primeira calc" << endl;
+    //cout << "Digite o primeiro número: " << endl;
+    //cin >> num1;
+
+    //cout << "Digite o segundo número: " << endl;
+    //cin >> num2;
+
+    //cout << "A soma é: " << num1 + num2 << endl;
+    //cout << "A subtração é: " << num1 - num2 << endl;
+    //cout << "O produto é: " << num1 * num2 << endl;
+    //cout << "A divisão é: " << num1 / num2 << endl;
+
+
+    //Aula 19
+    // Módulo da Divisão
+
+    //int a = 5 / 2;
+    //int num1, num2;
+    //num1 = 10;
+    //num2 = 3;
+
+    //int div = num1 / num2;
+    //int resto = num1 % num2;
+
+    //cout << "A divisão de num1 por num2 é igual a: " << div << endl;
+    //cout << "A divisão de num1 por num2 tem resto: " << resto << endl;
+
+
+    //Aula 23
+    // Tomada de decisão 3
+
+    //double nota1, nota2, nota3, nota4;
+
+    //cout << "Digite a sua primeira nota do semestre >>> ";
+    //cin >> nota1;
+
+    //cout << "Digite a sua segunda nota do semestre >>> ";
+    //cin >> nota2;
+
+    //cout << "Digite a sua terceira nota do semestre >>> ";
+    //cin >> nota3;
+
+    //cout << "Digite a sua quarta nota do semestre >>> ";
+    //cin >> nota4;
+
+    //double soma = nota1 + nota2 + nota2 + nota4;
+    //soma = soma /4;
+
+    //if(soma >= 7)
+    //{
+      //  cout << "Parabéns, você foi aprovado, sua nota foi: " << soma << endl;
+    //}
+    //else
+    //{
+        //cout << "Não deu, tente na próxima, sua nota foi: " << soma << endl;
+    //}
+
+
+    // Aula 24
+    // Operadores Lógicos
+
+
+
 
 
     system("pause");
